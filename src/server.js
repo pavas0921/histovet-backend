1dcac7f5118caa5fee03ecbe709ea06c1cd5fd30
Andres Pavas <andrespavas@MacBook-Air-de-Andres.local>
2025-04-24 23:24:04 -0500

    lamba file implementation to deploy in aws lambda service

--- src/server.js
+++ src/server.js
@@ -2,8 +2,14 @@ import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(PORT);
-});
+// Solo inicia el servidor si no está en Lambda
+if (!process.env.LAMBDA_TASK_ROOT) {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+}
+
+// Exporta la app para pruebas o uso en Lambda
+export default app;
